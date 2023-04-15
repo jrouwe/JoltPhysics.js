7ec3bfcc2ee36c8b0f1912424140e9dc79507fe1
Isaac Mason <isaac@isaacmason.com>
2023-04-15 18:18:11 +1000

    npm config, esm build, types, inlined base64 build (#5)

--- build.sh
+++ build.sh
@@ -10,3 +10,18 @@ fi
 
 cmake -B Build/$BUILD_TYPE -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build Build/$BUILD_TYPE -j`nproc`
+
+npx webidl-dts-gen -e -d -i ./JoltJS.idl -o ./Build/types.d.ts -n Jolt
+
+cat > ./Build/jolt-physics.d.ts << EOF
+import Jolt from "./types";
+
+export default Jolt;
+export * from "./types";
+
+EOF
+
+cp ./Build/jolt-physics.d.ts ./Build/jolt-physics.wasm.d.ts
+cp ./Build/jolt-physics.d.ts ./Build/jolt-physics.wasm-compat.d.ts
+
+cp ./Build/jolt-physics.wasm-compat.js ./Examples/js/jolt-physics.wasm-compat.js
--- onload.js
+++
@@ -1,1 +0,0 @@
-this['Jolt'] = Module;
