3c4b7f1530b50eb9e377417710d86eda84ec2d03
Jorrit Rouwe <jrouwe@gmail.com>
2023-11-27 20:47:13 +0100

    Improved documentation

--- Examples/js/example.js
+++ Examples/js/example.js
@@ -27,6 +27,7 @@ const unwrapQuat = (q) => new Jolt.Quat(q.x, q.y, q.z, q.w);
 // Object layers
 const LAYER_NON_MOVING = 0;
 const LAYER_MOVING = 1;
+const NUM_OBJECT_LAYERS = 2;
 
 function getRandomQuat() {
 	return Jolt.Quat.prototype.sRotation(new Jolt.Vec3(0.001 + Math.random(), Math.random(), Math.random()).Normalized(), 2 * Math.PI * Math.random());
@@ -70,15 +71,22 @@ function initGraphics() {
 }
 
 function initPhysics() {
-	// We use only 2 layers: one for non-moving objects and one for moving objects
-	let objectFilter = new Jolt.ObjectLayerPairFilterTable(2);
+	// Layer that objects can be in, determines which other objects it can collide with
+	// Typically you at least want to have 1 layer for moving bodies and 1 layer for static bodies, but you can have more
+	// layers if you want. E.g. you could have a layer for high detail collision (which is not used by the physics simulation
+	// but only if you do collision testing).
+	let objectFilter = new Jolt.ObjectLayerPairFilterTable(NUM_OBJECT_LAYERS);
 	objectFilter.EnableCollision(LAYER_NON_MOVING, LAYER_MOVING);
 	objectFilter.EnableCollision(LAYER_MOVING, LAYER_MOVING);
 
-	// We use a 1-to-1 mapping between object layers and broadphase layers
+	// Each broadphase layer results in a separate bounding volume tree in the broad phase. You at least want to have
+	// a layer for non-moving and moving objects to avoid having to update a tree full of static objects every frame.
+	// You can have a 1-on-1 mapping between object layers and broadphase layers (like in this case) but if you have
+	// many object layers you'll be creating many broad phase trees, which is not efficient.
 	const BP_LAYER_NON_MOVING = new Jolt.BroadPhaseLayer(0);
 	const BP_LAYER_MOVING = new Jolt.BroadPhaseLayer(1);
-	let bpInterface = new Jolt.BroadPhaseLayerInterfaceTable(2, 2);
+	const NUM_BROAD_PHASE_LAYERS = 2;
+	let bpInterface = new Jolt.BroadPhaseLayerInterfaceTable(NUM_OBJECT_LAYERS, NUM_BROAD_PHASE_LAYERS);
 	bpInterface.MapObjectToBroadPhaseLayer(LAYER_NON_MOVING, BP_LAYER_NON_MOVING);
 	bpInterface.MapObjectToBroadPhaseLayer(LAYER_MOVING, BP_LAYER_MOVING);
 
@@ -86,7 +94,7 @@ function initPhysics() {
 	settings = new Jolt.JoltSettings();
 	settings.mObjectLayerPairFilter = objectFilter;
 	settings.mBroadPhaseLayerInterface = bpInterface;
-	settings.mObjectVsBroadPhaseLayerFilter = new Jolt.ObjectVsBroadPhaseLayerFilterTable(settings.mBroadPhaseLayerInterface, 2, settings.mObjectLayerPairFilter, 2);
+	settings.mObjectVsBroadPhaseLayerFilter = new Jolt.ObjectVsBroadPhaseLayerFilterTable(settings.mBroadPhaseLayerInterface, NUM_BROAD_PHASE_LAYERS, settings.mObjectLayerPairFilter, NUM_OBJECT_LAYERS);
 	jolt = new Jolt.JoltInterface(settings);
 	physicsSystem = jolt.GetPhysicsSystem();
 	bodyInterface = physicsSystem.GetBodyInterface();
