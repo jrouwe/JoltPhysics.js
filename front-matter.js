28595e9fe614533c9ffafc4792048559524dd0a3
Jorrit Rouwe <jrouwe@gmail.com>
2024-01-12 15:53:09 +0100

    Updated year

--- front-matter.js
+++ front-matter.js
@@ -1,3 +1,3 @@
-// SPDX-FileCopyrightText: 2022 Jorrit Rouwe
+// SPDX-FileCopyrightText: 2022-2024 Jorrit Rouwe
 // SPDX-License-Identifier: MIT
 // This is Web Assembly version of Jolt Physics, see: https://github.com/jrouwe/JoltPhysics.js
